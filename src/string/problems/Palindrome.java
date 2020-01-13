1ed9db5b822439d3786dc3f43a968cc24b1f23d9
Robert <robertavila33@gmail.com>
2020-01-12 20:03:00 -0500

    Completed Palindrome

--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -7,5 +7,20 @@ public class Palindrome {
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
 
+        String Word = "poop";
+        String Reverse = "";
+        //Boolean Palindrome = true;
+
+
+        for(int i = Word.length() - 1; i >= 0; i-- ) {
+            Reverse += Word.charAt(i);
+            System.out.println(Reverse);
+        }
+
+       if (Reverse.equalsIgnoreCase(Word)){
+            System.out.println("PALINDROME!");
+        } else {
+            System.out.println("No a Palindrome!");
+        }
     }
 }
