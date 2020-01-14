5bed4664278b47917c368e23fd38dcbce3d568c7
Robert <robertavila33@gmail.com>
2020-01-14 13:37:34 -0500

    Completed Arraylist

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,13 @@
 package datastructure;
 
+import databases.ConnectToSqlDB;
+
+import javax.swing.text.html.HTMLDocument;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
 public class UseArrayList {
 
 	public static void main(String[] args) {
@@ -9,7 +17,53 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
-	
+
+			//Creating object of Arraylist and stored values
+		ArrayList<String> al= new ArrayList<String>();
+
+
+		al.add("a");
+		al.add("f");
+		al.add("c");
+		al.add("d");
+		al.add("z");
+		al.add("f");
+		al.add("w");
+		al.add("h");
+		al.add("n");
+		System.out.println("There are " + al + " within this list!" + "\n");
+
+		al.remove(8);
+		al.remove(7);
+
+
+
+		// For each loop
+		for(String obj:al){
+			System.out.println(al + "\n");
+		}
+		al.remove(6);
+		al.remove(5);
+
+		System.out.println("Now the last letter is " + al.get(4) + "\n");
+
+		Iterator<String> iter = al.iterator();
+		while(iter.hasNext()){
+			System.out.println(iter.next());
+		}
+
+			// Sorting it
+		Collections.sort(al);
+		System.out.println(al);
+
+			//Storing ini database
+		//ConnectToSqlDB.connectToSqlDatabase();
+		ConnectToSqlDB DB = new ConnectToSqlDB();
+		//DB.insertDataFromArrayToSqlTable();
+		//insertDataFromArrayToSqlTable();
+
+
+		//insertDataFromArrayListToSqlTable();		//(List<Student> list, String tableName, String columnName)
 
 	}
 
