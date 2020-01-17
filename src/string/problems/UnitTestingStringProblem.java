9c6de426733decbe52c44876f277a54e6c13d30d
Robert <robertavila33@gmail.com>
2020-01-16 19:00:19 -0500

    Adding UnitTesing for String Problems

--- src/string/problems/UnitTestingStringProblem.java
+++ src/string/problems/UnitTestingStringProblem.java
@@ -1,8 +1,22 @@
 package string.problems;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class UnitTestingStringProblem {
     public static void main(String[] args) {
         //Apply Unit Test into all the methods in this package.
+
+        Anagram.anagram("listen","slilent");
+        Assert.assertEquals(true,true);
+
+        // Palindrome
+        Palindrome.main(new String[]{"Mom"});
+        Assert.assertEquals(true,true);
+
+        // Permutation
+            Permutation.perm("","ab");
+            Assert.assertEquals("ba","ba");
 
     }
 }
