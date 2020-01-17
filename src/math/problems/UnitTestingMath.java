556abd580373289c3a7c608ee4fa58b863808e2b
Robert <robertavila33@gmail.com>
2020-01-16 22:02:15 -0500

    Adding UnitTestingMath

--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -1,8 +1,20 @@
 package math.problems;
 
+import org.testng.Assert;
+
 public class UnitTestingMath {
     public static void main(String[] args) {
         //Apply Unit testing into each classes and methods in this package.
+
+        //Factorial
+        Factorial.factor(5);
+       Assert.assertEquals(120, 120);
+
+        // Find Lowest Number
+
+
+        // Make Pyramid
+        MakePyramid.pyramid( );
 
     }
 }
