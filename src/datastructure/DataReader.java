be4a9e7a8cd1f56a802c9245802bc2cc6a422459
Robert <robertavila33@gmail.com>
2020-01-17 16:12:27 -0500

    Adding DataReader

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -1,5 +1,13 @@
 package datastructure;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class DataReader {
 
 	public static void main(String[] args) {
@@ -19,7 +27,76 @@ public class DataReader {
 		 */
 
 		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+		System.out.println(textFile);
+		BufferedReader buffR = null;	//buffer
+		String fileText = null;		//reader
 
+
+		//Datastructures
+		Stack<String> stack = new Stack<String>();
+		LinkedList<String> list = new LinkedList<String>();
+
+		// Creating and place reader into buffer
+		//Catching exception
+		try {
+			FileReader filerR = new FileReader(textFile);
+			buffR = new BufferedReader(filerR);
+
+		}catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
+
+				//Reading lines. Catching Exceptions
+		try{
+
+			fileText = buffR.readLine();	// Worked
+		} catch (IOException |NullPointerException e ){
+			System.out.println(e);
+		}
+
+				//Splitting Catching exceptions
+		try {
+			String [] words = fileText.split(":");
+
+			for(int i=0;i<words.length;i++) {
+
+				System.out.println(words[i]);
+
+				stack.push(words[i]);			//?????
+
+				list.add(words[i]);				//?????
+
+			}
+		} catch (NullPointerException e){
+			System.out.println(e);
+		}
+
+		Iterator iter = list.iterator();
+
+		System.out.println("Linked List Display:");
+		while (iter.hasNext()){
+			System.out.println(iter.next());
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+		//Iterator iter = list.iterator();
+
+		//System.out.println("Linked List Display:");
+
+		//while (iter.hasNext())
+
+		//	System.out.println(iter.next());
 
 
 	}
