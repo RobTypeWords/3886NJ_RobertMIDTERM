f8f706ac95238038cc28475717ef6265c4c3e2be
Robert <robertavila33@gmail.com>
2020-01-16 19:05:42 -0500

    Adding Abstract class

--- src/design/aba.java
+++ src/design/aba.java
@@ -1,4 +1,26 @@
 package design;
 
-public class aba {
+public abstract class aba implements Employee {
+
+    final String noobie = "Jimmy";
+    static int RatePerformance;
+
+            // Abstract Method
+
+    public abstract void welcome();
+
+    // Implementation of methods  from Employee Interface
+
+
+    public void assignDepartment(String noobie) {
+        System.out.println(noobie);
+    }
+
+    public void benefitLayout(String noobie) {
+        System.out.println(noobie +", we will offer great benefits" );
+    }
+
+
 }
+
+
